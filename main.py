a4262bd06a256d673727aa344089622c750ab157
Amiran Ivgi <109898333+AmiranIV@users.noreply.github.com>
2023-08-14 14:50:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,12 @@ s3.put_object(Bucket=BUCKET,
               SSECustomerKey=KEY,
               SSECustomerAlgorithm='AES256') #Encryption Algorithm used
 print("Done")
+
+""" Bringing Back the Object """"
+print("Getting S3 object Created earlier...")
+response = s3.get_object(Bucket=BUCKET,#Same Bucket Name
+                         Key='NAME OF FILE IN S3',  #same key file 
+                         SSECustomerKey=KEY, #same KEY
+                         SSECustomerAlgorithm='AES256') #Same Encryption Algorithm Used
+print("Done, response body:")
+print(response['Body'].read())
